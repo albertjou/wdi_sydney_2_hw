1338e117a9579e440cc4ce3ea663cc1b89bf2720
Albertjou <albert_jou@hotmail.com>
2013-10-02 22:55:37 +1000

    Day 3 homework - MTA trains

--- w01/d03/albert/MTA.rb
+++ w01/d03/albert/MTA.rb
@@ -1,99 +1,123 @@
 #Title: MTA Lab
 
-# The list of stations for each line
+# This method outputs the stations required to get from A --> B (on the same line)
 
-n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
-l_line = ["8th", "6th", "Union Square", "3rd", "1st"]
-six_line = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
-
-
-
-# The menu
-puts "Which line do you want to start from:"
-starting_point_line = gets.chomp
-  case starting_point_line
-  when "n_line"
-    puts "You have selected the N line"
-    puts "What station do you want to start at"
-    station_start = gets.chomp
-    station_start_index = n_line.index(station_start)
-    puts "Are you staying on the same line?"
-    same_line = gets.chomp
-      if same_line == "yes"
-        puts "What station do you want to get off at"
-        station_end = gets.chomp
-        station_end_index = n_line.index(station_end)
-        puts n_line[station_start_index .. station_end_index]
-      elsif same_line == "no"
-        puts "What line do you want to stop at"
-        station_end_line = gets.chomp
-        puts "What stop do you want to get to"
-        station_end = gets.chomp
-        puts n_line[station_start_index ..4]
-        puts "Now change to the #{station_end_line}"
-        puts
-      else
-        puts "Sorry, I don't understand"
-      end
-
-
-  when "l_line"
-    puts "You have selected the L line"
-    puts "What station do you want to start at"
-    station_start = gets.chomp
-    station_start_index = l_line.index(station_start)
-    puts "Are you staying on the same line?"
-    same_line = gets.chomp
-      if same_line == "yes"
-        puts "What station do you want to get off at"
-        station_end = gets.chomp
-        station_end_index = l_line.index(station_end)
-        puts l_line[station_start_index .. station_end_index]
-      elsif same_line == "no"
-        puts "What line do you want to stop at"
-        station_end_line = gets.chomp
-        puts "What stop do you want to get to"
-        station_end = gets.chomp
-        puts l_line[station_start_index ..4]
-      else
-        puts "Sorry, I don't understand"
-      end
-  when "six_line"
-    puts "You have selected the 6 line"
-    puts "What station do you want to start at"
-    station_start = gets.chomp
-    station_start_index = six_line.index(station_start)
-    puts "Are you staying on the same line?"
-    same_line = gets.chomp
-      if same_line == "yes"
-        puts "What station do you want to get off at"
-        station_end = gets.chomp
-        station_end_index = n_line.index(station_end)
-        puts six_line[station_start_index .. station_end_index]
-      elsif same_line == "no"
-        puts "What line do you want to stop at"
-        station_end_line = gets.chomp
-        puts "What stop do you want to get to"
-        station_end = gets.chomp
-        puts six_line[station_start_index ..4]
-      else
-        puts "Sorry, I don't understand"
-      end
+def same_line(line, start_index, end_index)
+  if (start_index > end_index)
+    start_index = reverse_direction(line, start_index)
+    end_index = reverse_direction(line, end_index)
+    line.reverse!
+    line[start_index..end_index]
+  elsif start_index == end_index
+    "\nBuddy, have a look around, you are already there...\n"
   else
-    puts "sorry I don't recognise that line"
+    line[start_index..end_index]
   end
+end
+
+#This method is where the data for subway lines are stored.
+# It also takes an input for the line and returns the array of stations for the relevant line.
+# TO DO: UNKNOWN INPUT HANDLING
+
+def journey_line()
+  subway_system = {
+    :n_line => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+    :l_line => ["8th", "6th", "Union Square", "3rd", "1st"],
+    :six_line => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+  }
+  puts "N Line stations (n_line):\n #{subway_system[:n_line]}"
+  puts ""
+  puts "L Line stations (l_line):\n #{subway_system[:l_line]}"
+  puts ""
+  puts "6 Line stations (six_line):\n #{subway_system[:six_line]}"
+  print "\nLine?\t\t"
+  return subway_system[gets.chomp.to_sym]
+end
+
+# This method asks for the station and converts it into an index number.
+# TO DO: UNKNOWN INPUT HANDLING
+
+def journey_station(line)
+  print "Station?\t"
+  return line.index(gets.chomp)
+end
+
+# Reverses the value's array index number.
+def reverse_direction(line, index)
+  return line.reverse.index(line[index])
+end
+
+# This is just a bit of fun...
+
+def draw_me_a_train
+  puts %Q{            .---- -  -
+   (   ,----- - -
+    \_/      ___
+    c--U---^--'o  [_
+    |------------'_|   MTA ROUTE OPTIMIZER
+    /_(o)(o)--(o)(o)    *******************
+    ~ ~~~~~~~~~~~~~~~~~~~~~~~~
+  }
+end
+
+system "clear" unless system "cls"
+
+draw_me_a_train
+puts "Where do you want to start:"
+puts ""
+start_line = journey_line
+start_index = journey_station(start_line)
+print "Do you want to stay on the same line? (Y/N)?  "
+response = gets.chomp.upcase
+if response[0] == "Y"
+  end_index = journey_station(start_line)
+  print "\nThe trip from #{start_line[start_index]} to #{start_line[end_index]} is #{same_line(start_line, start_index, end_index).count} stops total:"
+  puts ""
+  print same_line(start_line, start_index, end_index)
+elsif response[0] == "N"
+  puts ""
+  end_index = start_line.index("Union Square")
+  part_one = same_line(start_line, start_index, end_index)
+  puts "Which line do you want to go to:"
+  end_line = journey_line
+  if end_line == start_line
+    puts "You are already on that line, which other line do you want to go to?"
+    end_line = journey_line
+  end
+  end_index = journey_station(end_line)
+  # puts "First you travel to Union Square, the "
+  start_index_new = end_line.index("Union Square")
+  part_two = same_line(end_line, start_index_new, end_index)
+  part_two.shift
+  print "\nThe trip from #{part_one.first} to #{part_two.last} is #{(part_one+part_two).count} stops total:"
+  puts ""
+  print part_one + part_two
+else
+  puts "Sorry, I only take yes/no answers."
+  quit
+end
+puts ""
+puts "Have a safe trip\n\n\n"
+
+# Extension
+
+# What happens if there is another line that touches n_line at 23rd and six_line at Astor Place
+
+# subway_system_new = {
+#   :my_line => ["23rd", "Happy Land", "Zombieville", "Astor Place", "End of the line"]
+#   :n_line => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
+#   :l_line => ["8th", "6th", "Union Square", "3rd", "1st"],
+#   :six_line => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+# }
+
+# We would need to create a table of connections?
+
+# Select the shortest route that goes to either 23rd @ n_line or Astor Place @ six_line
 
 
-  # puts "What about the station?"
-  # starting_point = n_line.index(gets.chomp.to_s)
-  # # puts "Which line do you want to end at:"
-  # # end_point_line = gets.chomp
-  # # puts "What about the station?"
-  # # end_point = end_point_line.index(gets.chomp)
-  # # puts "#{end_point}"
 
-  # # if starting_point_line == end_point_line
-  #   puts "#{starting_point_line[starting_point]}"
-  # # else
-  # #   puts "Ok"
-  # # end
+# Extension extension. If there were multiple connections, we would aim to prioritise minimum switches
+
+
+
+
