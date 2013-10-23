49cf32bd3ebf2aaec599525709188ddaf9d821c9
albertjou <albert_jou@hotmail.com>
2013-10-23 17:08:29 +1100

    Artwork homework

--- w03/d05/albert/artworks.sql
+++ w03/d05/albert/artworks.sql
@@ -2,8 +2,7 @@ CREATE TABLE artworks (
   id SERIAL4 PRIMARY KEY,
   name TEXT,
   image TEXT,
-  year DATE,
-  price MONEY,
-  artist_id SERIAL4,
-  FOREIGN KEY (artist_id) REFERENCES artists (id)
+  height SMALLINT,
+  country TEXT,
+  deaths SMALLINT
 );
